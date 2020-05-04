b6c67317f9ddfa133102eee24dbbe6d2f4b37815
Nico <pinganico@gmail.com>
2020-05-04 15:59:14 -0300

    BS4 filtra el link de descarga, vamos mejorando

--- Download_pdf_mati.py
+++ Download_pdf_mati.py
@@ -7,7 +7,7 @@ from random import randint
 countlineas = 0
 countlinks = 0
 rdm = randint(1,400)
-print(rdm)
+#print(rdm)
 # https://docs.google.com/spreadsheets/d/1HzdumNltTj2SHmCv3SRdoub8SvpIEn75fa4Q23x0keU/htmlview?fbclid=IwAR10EFL8JMRtaqfbx8f3ccEFu5hs-XGxChRNcYxs1N3Z_ztAG2F3eEblkvw
 link = "https://docs.google.com/spreadsheets/d/1HzdumNltTj2SHmCv3SRdoub8SvpIEn75fa4Q23x0keU/htmlview?fbclid=IwAR10EFL8JMRtaqfbx8f3ccEFu5hs-XGxChRNcYxs1N3Z_ztAG2F3eEblkvw"
 # link de mati donde esta la lista de libros
@@ -19,38 +19,36 @@ def leerhtmltxt(option):
 source = leerhtmltxt(link)
 #source = source.split()
 
-print(type(source))
+#print(type(source))
 source = re.sub(r"&amp;","&",source)
 # necesito copiar esto \ ^
 x = re.findall(r"(http://lin+[\w\./?&=;-]+)",str(source))
 
 # link q anda   http://link.springer.com/openurl?genre=book&isbn=978-3-319-26551-3
 #link q no anda http://link.springer.com/openurl?genre=book&amp;isbn=978-3-319-26551-3
-print(x[390])
-print("findall encontro ", len(x), "links para descargar")
+#print(x[390])
+#print("findall encontro ", len(x), "links para descargar")
 paradescarga = leerhtmltxt(x[390])
 pd = paradescarga.split()
 
 #intentando hacer andar BS4
 soup = BeautifulSoup(paradescarga, "html.parser")
-lincona = soup.a
+tlincona = soup.find_all("a")
+
+#hay que crear una lista para agregar links de descarga:
+ldown = [] 
+print(type(ldown))
 print("Link BS4")
-print(lincona) 
+for line in tlincona:     
+        #esto devuelve todos los title
+        #print(line.get( "title"))
+        #vamos a filtrarlos
+        if line.get("title") == None: continue
+        if line.get("title") != "Download this book in PDF format": continue 
+        sacalink = str(line.get( "href"))
+        if sacalink not in ldown: ldown.append(sacalink)
+print(ldown)
 
-print("Sin BS4")
-for line in pd:
-        if len(line) < 30:continue
-        if re.search("content/pdf",line):
-                print(line)
-#for line in source:
-#    if len(line) < 25: continue
-#    countlineas += 1
-#    if re.findall("http",line):
-#        print(line)
-#        countlinks += 1
-#    if re.search(r"http",line):
-#        print(line)
-#        count = count + 1 
-
-# fin de la pag </div>
-#print("Econtramos " , countlinks , "lineas.")
+#hasta aca, ya devuelve el link de descarga sin el home
+#y lo agrega a la lista sin duplicar (porq en la pagina
+#estan dos veces los links
