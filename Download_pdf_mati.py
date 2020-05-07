533a7ff59940e19d6ffc73d1cb749d01d16588a4
Nico <pinganico@gmail.com>
2020-05-07 17:09:40 -0300

    Ordenado y explicado. y ya baja el archivo con su nombre

--- Download_pdf_mati.py
+++ Download_pdf_mati.py
@@ -1,13 +1,12 @@
 
 from bs4 import BeautifulSoup
-
 import requests
 import re
 from random import randint
-countlineas = 0
-countlinks = 0
+
 rdm = randint(1,400)
-print(rdm)
+
+print("Bajamos el libro #: " + rdm)
 # https://docs.google.com/spreadsheets/d/1HzdumNltTj2SHmCv3SRdoub8SvpIEn75fa4Q23x0keU/htmlview?fbclid=IwAR10EFL8JMRtaqfbx8f3ccEFu5hs-XGxChRNcYxs1N3Z_ztAG2F3eEblkvw
 link = "https://docs.google.com/spreadsheets/d/1HzdumNltTj2SHmCv3SRdoub8SvpIEn75fa4Q23x0keU/htmlview?fbclid=IwAR10EFL8JMRtaqfbx8f3ccEFu5hs-XGxChRNcYxs1N3Z_ztAG2F3eEblkvw"
 # link de mati donde esta la lista de libros
@@ -16,31 +15,33 @@ def leerhtmltxt(option):
         L = requests.get(option).text
         return L
 
-source = leerhtmltxt(link)
-#source = source.split()
 
-#print(type(source))
+source = leerhtmltxt(link)
+
+#el & lo lee como &amp asi que hay q borrar las entradas erroneas
 source = re.sub(r"&amp;","&",source)
-# necesito copiar esto \ ^
+
+# necesito copiar esto \ ^ porq no se escribirlo con el teclado
 x = re.findall(r"(http://lin+[\w\./?&=;-]+)",str(source))
 
-# link q anda   http://link.springer.com/openurl?genre=book&isbn=978-3-319-26551-3
-#link q no anda http://link.springer.com/openurl?genre=book&amp;isbn=978-3-319-26551-3
-#print(x[390])
-#print("findall encontro ", len(x), "links para descargar")
 paradescarga = leerhtmltxt(x[rdm])
+
 #hay que buscar el home porq en el link de descarga le falta
 home = re.findall(r"(http?://\S+)/",x[rdm])
-pd = paradescarga.split()
-#print(x[3])
+
 #intentando hacer andar BS4
 soup = BeautifulSoup(paradescarga, "html.parser")
+
+#sacamos el titulo del libro para el archivo PDF
+tit = soup.title.string
+tit = tit[0:tit.index("|")-1]
+
 tlincona = soup.find_all("a")
 
 #hay que crear una lista para agregar links de descarga:
 ldown = [] 
-#print(type(ldown))
-#print("Link BS4")
+
+#pasamos las lineas para sacar el link de descarga
 for line in tlincona:     
         #esto devuelve todos los title
         #print(line.get( "title"))
@@ -56,5 +57,9 @@ for line in tlincona:
 #y lo agrega a la lista sin duplicar (porq en la pagina
 #estan dos veces los links
 
+print("Se va a crear: "+"/home/nico/Descargas/Mati-PDF/" + tit + ".pdf")
+
+
+
 lfinal = requests.get(ldown[0])
-open("/home/nico/Descargas/Mati-PDF/pdf1.pdf" , "wb").write(lfinal.content)
+open("/home/nico/Descargas/Mati-PDF/" + tit + ".pdf" , "wb").write(lfinal.content)
